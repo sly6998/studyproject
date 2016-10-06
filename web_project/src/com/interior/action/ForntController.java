3822666e97f471df6afada9668b9a96da2221d7f
sly6998 <sly6998@naver.com>
2016-10-06 18:16:14 +0900

    컨트롤러 생성 - 박종환 /10/06/18:16

---
+++ web_project/src/com/interior/action/ForntController.java
@@ -0,0 +1,100 @@
+package com.interior.action;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/ForntController")
+public class ForntController extends javax.servlet.http.HttpServlet implements javax.servlet.Servlet {
+
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String RequestURI = request.getRequestURI();
+		String contextPath = request.getContextPath();
+		String command = RequestURI.substring(contextPath.length());
+		ActionForward forward = null;
+		Action action = null;
+
+		if (command.equals("")) {
+
+		} else if (command.equals("")) {
+
+		} else if (command.equals("")) {
+
+		} else if (command.equals("")) {
+
+		} else if (command.equals("")) {
+
+		} else if (command.equals("")) {
+
+		} else if (command.equals("")) {
+
+		} else if (command.equals("")) {
+
+		} else if (command.equals("")) {
+
+		} else if (command.equals("")) {
+
+		} else if (command.equals("")) {
+
+		} else if (command.equals("")) {
+
+		} else if (command.equals("")) {
+
+		} else if (command.equals("")) {
+
+		} else if (command.equals("")) {
+
+		} else if (command.equals("")) {
+
+		} else if (command.equals("")) {
+
+		} else if (command.equals("")) {
+
+		} else if (command.equals("")) {
+
+		} else if (command.equals("")) {
+
+		} else if (command.equals("")) {
+
+		} else if (command.equals("")) {
+
+		} else if (command.equals("")) {
+
+		} else if (command.equals("")) {
+
+		} else if (command.equals("")) {
+
+		} else if (command.equals("")) {
+
+		}
+
+		if (forward != null) {
+			if (forward.isRedirect()) {
+				response.sendRedirect(forward.getPath());
+			} else {
+				RequestDispatcher dispatcher = request.getRequestDispatcher(forward.getPath());
+				dispatcher.forward(request, response);
+			}
+		}
+	}
+
+	
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doProcess(request, response);
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doProcess(request, response);
+	}
+
+}
