8630ae17228ea94b210376869006e32ed4eefbf6
sly6998 <sly6998@naver.com>
2016-10-12 13:08:46 +0900

    질문게시판 컨트롤러 & action 중간 commit - 박종환

--- web_project/src/com/interior/controller/FrontController.java
+++ web_project/src/com/interior/controller/FrontController.java
@@ -1,6 +1,5 @@
 package com.interior.controller;
 
-
 import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
@@ -25,202 +24,267 @@ import com.interior.noti.NotiListAction;
 import com.interior.noti.NotiModifyAction;
 import com.interior.noti.NotiModifyViewAction;
 import com.interior.noti.NotiWriteAction;
-
+import com.interior.qna.QnaDetailAction;
+import com.interior.qna.QnaListAction;
 
 @WebServlet("/ForntController")
 public class FrontController extends javax.servlet.http.HttpServlet implements javax.servlet.Servlet {
-  private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
 	protected void doProcess(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-	  
+
 		String RequestURI = request.getRequestURI();
 		String contextPath = request.getContextPath();
 		String command = RequestURI.substring(contextPath.length());
 		ActionForward forward = null;
 		Action action = null;
+
 		
 		
 		
-		/* Move */
 		
-		if (command.equals("/mainpage.html")) {//메인페이지 이동
-		  forward = new ActionForward();
-		  forward.setRedirect(false);
-		  forward.setPath("./page/mainpage.jsp");
+		/* Main */
 
-		} else if (command.equals("/login.html")) {//로그인페이지 이동
-			  forward = new ActionForward();
-			  forward.setRedirect(false);
-			  forward.setPath("./page/login.jsp");
-
-		}else if (command.equals("/join.html")) {//회원가입 페이지 이동
-			  forward = new ActionForward();
-			  forward.setRedirect(false);
-			  forward.setPath("./member/member_join.jsp");
-
-		}else if (command.equals("/emailfind.html")) {//이메일 찾기 페이지 이동
-			  forward = new ActionForward();
-			  forward.setRedirect(false);
-			  forward.setPath("./member/member_find_email.jsp");
-
-		}else if (command.equals("/pwdfind.html")) {//비밀번호 찾기 페이지 이동
-			  forward = new ActionForward();
-			  forward.setRedirect(false);
-			  forward.setPath("./member/member_find_pw.jsp");
-
-		}else if (command.equals("/memberdelete.html")) {//회원탈퇴 페이지 이동
-			  forward = new ActionForward();
-			  forward.setRedirect(false);
-			  forward.setPath("./member/member_delete.jsp");
-
-		}else if (command.equals("/member_modify.html")) {//마이페이지 이동 & action
-			action = new MemberModifyViewAction();
-		      try {
-		       forward = action.execute(request, response);
-		     } catch (Exception e) {
-		       e.printStackTrace();
-		     }
-		}else if (command.equals("/member_view.html")) {//회원정보 보기 페이지 이동 & action
-			action = new MemberViewAction();
-		      try {
-		       forward = action.execute(request, response);
-		     } catch (Exception e) {
-		       e.printStackTrace();
-		     }
-		}else if (command.equals("/member_management.html")) {//회원관리 페이지 이동 & action(관리자)
-			action = new MemberManagementAction();
-		      try {
-		       forward = action.execute(request, response);
-		     } catch (Exception e) {
-		       e.printStackTrace();
-		     }
-		}else if (command.equals("/noti_list.html")) {//공지사항페이지 이동 & action
-			action = new NotiListAction();
-		      try {
-		       forward = action.execute(request, response);
-		     } catch (Exception e) {
-		       e.printStackTrace();
-		     }
-		}else if (command.equals("/noti_detail.html")){ //공지사항 상세보기 페이지 이동 & action
-			action = new NotiDetailAction();
-		      try {
-		       forward = action.execute(request, response);
-		     } catch (Exception e) {
-		       e.printStackTrace();
-		     }
-		}else if (command.equals("/noti_write.html")){ //공지사항 글 쓰기 페이지 이동
+		if (command.equals("/mainpage.html")) {// 메인페이지 이동
 			forward = new ActionForward();
-			  forward.setRedirect(false);
-			  forward.setPath("./noti/noti_write.jsp");
-		}else if (command.equals("/noti_modify_view.html")){ //공지사항 글 수정 페이지 이동 & action
-			action = new NotiModifyViewAction();
-		      try {
-		       forward = action.execute(request, response);
-		     } catch (Exception e) {
-		       e.printStackTrace();
-		     }
-		}else if (command.equals("/catalogue.html")) {//카탈로그페이지 이동
-			  forward = new ActionForward();
-			  forward.setRedirect(false);
-			  forward.setPath("./catalogue/catalogue.jsp");
-		
-		} else if (command.equals("/product_list.html")) {//제품페이지 이동
-			  forward = new ActionForward();
-			  forward.setRedirect(false);
-			  forward.setPath("./product/product_list.jsp");
-		
-		} else if (command.equals("/review_list.html")) {//후기페이지 이동
-			  forward = new ActionForward();
-			  forward.setRedirect(false);
-			  forward.setPath("./review/review_list.jsp");
-		
-		} else if (command.equals("/qna_list.html")) {//질문게시판 이동
-			  forward = new ActionForward();
-			  forward.setRedirect(false);
-			  forward.setPath("./qnaboard/qna_list.jsp");
-		
-		} else if (command.equals("/advice_request.html")) {//상담신청페이지 이동
-			  forward = new ActionForward();
-			  forward.setRedirect(false);
-			  forward.setPath("./advice/advice_request.jsp");
+			forward.setRedirect(false);
+			forward.setPath("./page/mainpage.jsp");
 
-			  
-			  
-		/* Action */	  
-			  
-		} else if (command.equals("/MemberJoinAction.html")) {//회원가입 action
+		} else if (command.equals("/login.html")) {// 로그인페이지 이동
+			forward = new ActionForward();
+			forward.setRedirect(false);
+			forward.setPath("./page/login.jsp");
+
+			
+			
+			
+			
+		/* MEMBER */
+
+		} else if (command.equals("/join.html")) {// 회원가입 페이지 이동
+			forward = new ActionForward();
+			forward.setRedirect(false);
+			forward.setPath("./member/member_join.jsp");
+
+		} else if (command.equals("/emailfind.html")) {// 이메일 찾기 페이지 이동
+			forward = new ActionForward();
+			forward.setRedirect(false);
+			forward.setPath("./member/member_find_email.jsp");
+
+		} else if (command.equals("/pwdfind.html")) {// 비밀번호 찾기 페이지 이동
+			forward = new ActionForward();
+			forward.setRedirect(false);
+			forward.setPath("./member/member_find_pw.jsp");
+
+		} else if (command.equals("/memberdelete.html")) {// 회원탈퇴 페이지 이동
+			forward = new ActionForward();
+			forward.setRedirect(false);
+			forward.setPath("./member/member_delete.jsp");
+
+		} else if (command.equals("/member_modify.html")) {// 마이페이지 이동 & action
+			action = new MemberModifyViewAction();
+			try {
+				forward = action.execute(request, response);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else if (command.equals("/member_view.html")) {// 회원정보 보기 페이지 이동 & action
+			action = new MemberViewAction();
+			try {
+				forward = action.execute(request, response);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else if (command.equals("/member_management.html")) {// 회원관리 페이지 이동 & action(관리자)
+			action = new MemberManagementAction();
+			try {
+				forward = action.execute(request, response);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else if (command.equals("/MemberJoinAction.html")) {// 회원가입 action
 			action = new MemberJoinAction();
-		      try {
-		       forward = action.execute(request, response);
-		     } catch (Exception e) {
-		       e.printStackTrace();
-		     }
-		}  else if (command.equals("/MemberLoginAction.html")) {//로그인 action
+			try {
+				forward = action.execute(request, response);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else if (command.equals("/MemberLoginAction.html")) {// 로그인 action
 			action = new MemberLoginAction();
-		      try {
-		       forward = action.execute(request, response);
-		     } catch (Exception e) {
-		       e.printStackTrace();
-		     }
-		} else if (command.equals("/MemberFindEmailAction.html")) {//이메일 찾기 action
+			try {
+				forward = action.execute(request, response);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else if (command.equals("/MemberFindEmailAction.html")) {// 이메일 찾기 action
 			action = new MemberFindEmailAction();
-		      try {
-		       forward = action.execute(request, response);
-		     } catch (Exception e) {
-		       e.printStackTrace();
-		     }
+			try {
+				forward = action.execute(request, response);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 
-		} else if (command.equals("/MemberFindPWDAction.html")) {//비밀번호 찾기 action
+		} else if (command.equals("/MemberFindPWDAction.html")) {// 비밀번호 찾기 action
 			action = new MemberFindPWDAction();
-		      try {
-		       forward = action.execute(request, response);
-		     } catch (Exception e) {
-		       e.printStackTrace();
-		     }
+			try {
+				forward = action.execute(request, response);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 
-		} else if (command.equals("/MemberDeleteAction.html")) {//회원탈퇴 action
+		} else if (command.equals("/MemberDeleteAction.html")) {// 회원탈퇴 action
 			action = new MemberDeleteAction();
-		      try {
-		       forward = action.execute(request, response);
-		     } catch (Exception e) {
-		       e.printStackTrace();
-		     }
-		} else if (command.equals("/MemberModifyAction.html")) {//마이페이지 수정 action
+			try {
+				forward = action.execute(request, response);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else if (command.equals("/MemberModifyAction.html")) {// 마이페이지 수정 action
 			action = new MemberModifyAction();
-		      try {
-		       forward = action.execute(request, response);
-		     } catch (Exception e) {
-		       e.printStackTrace();
-		     }
-		} else if (command.equals("/MemberManagementModify.html")) {//회원정보 수정(관리자)
+			try {
+				forward = action.execute(request, response);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else if (command.equals("/MemberManagementModify.html")) {// 회원정보 수정(관리자)
 			action = new MemberManagementModifyAction();
-		      try {
-		       forward = action.execute(request, response);
-		     } catch (Exception e) {
-		       e.printStackTrace();
-		     }
-		} else if (command.equals("/NotiWriteAction.html")) {//공지사항 글쓰기(관리자)
+			try {
+				forward = action.execute(request, response);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			
+			
+			
+			/* 공지사항 */
+
+		} else if (command.equals("/noti_list.html")){// 공지사항페이지 이동 & action
+			action = new NotiListAction();
+			try {
+				forward = action.execute(request, response);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else if (command.equals("/noti_detail.html")) { // 공지사항 상세보기 페이지 이동 & action
+			action = new NotiDetailAction();
+			try {
+				forward = action.execute(request, response);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else if (command.equals("/noti_write.html")) { // 공지사항 글 쓰기 페이지 이동(관리자)
+			forward = new ActionForward();
+			forward.setRedirect(false);
+			forward.setPath("./noti/noti_write.jsp");
+		} else if (command.equals("/noti_modify_view.html")) { // 공지사항 글 수정 페이지 이동 & action(관리자)
+			action = new NotiModifyViewAction();
+			try {
+				forward = action.execute(request, response);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else if (command.equals("/NotiWriteAction.html")) {// 공지사항 글쓰기 & action(관리자)
 			action = new NotiWriteAction();
-		      try {
-		       forward = action.execute(request, response);
-		     } catch (Exception e) {
-		       e.printStackTrace();
-		     }
-		} else if (command.equals("/NotiModifyAction.html")) {//공지사항 글수정(관리자)
+			try {
+				forward = action.execute(request, response);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else if (command.equals("/NotiModifyAction.html")) {// 공지사항 글수정(관리자)
 			action = new NotiModifyAction();
-		      try {
-		       forward = action.execute(request, response);
-		     } catch (Exception e) {
-		       e.printStackTrace();
-		     }
-		}else if (command.equals("/NotiDeleteAction.html")) {//공지사항 글 삭제(관리자)
+			try {
+				forward = action.execute(request, response);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else if (command.equals("/NotiDeleteAction.html")) {// 공지사항 글 삭제(관리자)
 			action = new NotiDeleteAction();
-		      try {
-		       forward = action.execute(request, response);
-		     } catch (Exception e) {
-		       e.printStackTrace();
-		     }
+			try {
+				forward = action.execute(request, response);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			
+			
+			
+			
+			/* 카탈로그 */
+
+		} else if (command.equals("/catalogue.html")) {// 카탈로그페이지 이동
+			forward = new ActionForward();
+			forward.setRedirect(false);
+			forward.setPath("./catalogue/catalogue.jsp");
+
+			
+			
+			
+			
+			/* 제품 */
+
+		} else if (command.equals("/product_list.html")) {// 제품페이지 이동
+			forward = new ActionForward();
+			forward.setRedirect(false);
+			forward.setPath("./product/product_list.jsp");
+
+			
+			
+			
+			
+			/* 후기 */
+
+		} else if (command.equals("/review_list.html")) {// 후기페이지 이동
+			forward = new ActionForward();
+			forward.setRedirect(false);
+			forward.setPath("./review/review_list.jsp");
+
+			
+			
+			
+			
+			/* 질문게시판 */
+
+		} else if (command.equals("/qna_list.html")) {// 질문게시판 이동 & action
+			action = new QnaListAction();
+			try {
+				forward = action.execute(request, response);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else if (command.equals("/qna_detail.html")) {// 질문게시판 상세보기 페이지 이동 & action
+			action = new QnaDetailAction();
+			try {
+				forward = action.execute(request, response);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else if (command.equals("/qna_write.html")) {// 질문게시판 글쓰기 페이지 이동
+			forward = new ActionForward();
+			forward.setRedirect(false);
+			forward.setPath("./qna/qna_write.jsp");
+			
+		} else if (command.equals("/QnaWriteAction.html")) {// 질문게시판 글쓰기 action
+			action = new QnaWriteAction();
+			try {
+				forward = action.execute(request, response);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+
+			
+			
+			
+			/* 상담신청 */
+
+		} else if (command.equals("/advice_request.html")) {// 상담신청페이지 이동
+			forward = new ActionForward();
+			forward.setRedirect(false);
+			forward.setPath("./advice/advice_request.jsp");
+
+		
 		} else if (command.equals("")) {
 
 		} else if (command.equals("")) {
@@ -251,18 +315,16 @@ public class FrontController extends javax.servlet.http.HttpServlet implements j
 		}
 	}
 
-	
-	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-	  System.out.println("get방식");
+		System.out.println("get방식");
 		doProcess(request, response);
 
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-	  System.out.println("post방식");
+		System.out.println("post방식");
 		doProcess(request, response);
 	}
 
---
+++ web_project/src/com/interior/qna/QnaDAO.java
@@ -0,0 +1,27 @@
+package com.interior.qna;
+
+import java.util.List;
+
+public class QnaDAO {
+
+	public int getListCount() {//qna 게시판 리스트의 총 개시글 수 구하기 
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public List getQnaList(int page, int limit) {//qna 게시글 불러오기
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void setReadCountUpdate(int num) {//qna 게시글 조회수 업데이트
+		// TODO Auto-generated method stub
+		
+	}
+
+	public QnaBean getDetail(int num) {//qna 게시글 상세보기
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
