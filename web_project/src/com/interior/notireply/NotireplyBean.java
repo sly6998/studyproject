b0d900f917bd592f501d6ebdb600de8f836f9c1f
yunje <yunje@Jun>
2016-10-22 20:46:40 +0900

    기존의 reply 테이블 삭제, noti_reply 테이블생성, 코드 작성중, jsp페이지는 아직... noti_reply 부분 완성한 후에 다른 댓글 테이블이랑 dao 작성하도록 하겠습니다. - jun

---
+++ web_project/src/com/interior/notireply/NotireplyBean.java
@@ -0,0 +1,60 @@
+package com.interior.notireply;
+
+import java.sql.Date;
+
+public class NotireplyBean {
+	
+	private String NOTI_REPLY_MEMBER_ID;
+	private String NOTI_REPLY_CONTENT;
+	private Date NOTI_REPLY_DATE;
+	private int NOTI_REPLY_SEQ;
+	private int NOTI_REPLY_REF;
+	private int NOTI_REPLY_LEV;
+	private int NOTI_REPLY_NUM;
+	
+	
+	public String getNOTI_REPLY_MEMBER_ID() {
+		return NOTI_REPLY_MEMBER_ID;
+	}
+	public void setNOTI_REPLY_MEMBER_ID(String nOTI_REPLY_MEMBER_ID) {
+		NOTI_REPLY_MEMBER_ID = nOTI_REPLY_MEMBER_ID;
+	}
+	public String getNOTI_REPLY_CONTENT() {
+		return NOTI_REPLY_CONTENT;
+	}
+	public void setNOTI_REPLY_CONTENT(String nOTI_REPLY_CONTENT) {
+		NOTI_REPLY_CONTENT = nOTI_REPLY_CONTENT;
+	}
+	public Date getNOTI_REPLY_DATE() {
+		return NOTI_REPLY_DATE;
+	}
+	public void setNOTI_REPLY_DATE(Date nOTI_REPLY_DATE) {
+		NOTI_REPLY_DATE = nOTI_REPLY_DATE;
+	}
+	public int getNOTI_REPLY_SEQ() {
+		return NOTI_REPLY_SEQ;
+	}
+	public void setNOTI_REPLY_SEQ(int nOTI_REPLY_SEQ) {
+		NOTI_REPLY_SEQ = nOTI_REPLY_SEQ;
+	}
+	public int getNOTI_REPLY_REF() {
+		return NOTI_REPLY_REF;
+	}
+	public void setNOTI_REPLY_REF(int nOTI_REPLY_REF) {
+		NOTI_REPLY_REF = nOTI_REPLY_REF;
+	}
+	public int getNOTI_REPLY_LEV() {
+		return NOTI_REPLY_LEV;
+	}
+	public void setNOTI_REPLY_LEV(int nOTI_REPLY_LEV) {
+		NOTI_REPLY_LEV = nOTI_REPLY_LEV;
+	}
+	public int getNOTI_REPLY_NUM() {
+		return NOTI_REPLY_NUM;
+	}
+	public void setNOTI_REPLY_NUM(int nOTI_REPLY_NUM) {
+		NOTI_REPLY_NUM = nOTI_REPLY_NUM;
+	}
+
+	
+}
