94e91309f93841d47fa5f9dd67cd489832f8c711
yunje <yunje@Jun>
2016-10-24 11:35:15 +0900

    notiDAO에 댓글쓰기 추가, notiBean 수정완료..

--- web_project/src/com/interior/noti/NotiBean.java
+++ web_project/src/com/interior/noti/NotiBean.java
@@ -5,27 +5,33 @@ import java.sql.Date;
 public class NotiBean {
 
 	private String NOTI_MEMBER_ID;
+	private String NOTI_MEMBER_NAME;
 	private String NOTI_SUBJECT;
 	private String NOTI_CONTENT;
 	private int NOTI_READCOUNT;
 	private Date NOTI_DATE;
 	private int NOTI_NUM;
-	private String NOTI_MEMBER_NAME;
+	private String NOTI_REPLY_MEMBER_NAME;
+	private String NOTI_REPLY_MEMBER_ID;
+	private String NOTI_REPLY_CONTENT;
+	private Date NOTI_REPLY_DATE;
+	private int NOTI_REPLY_SEQ;
+	private int NOTI_REPLY_REF;
+	private int NOTI_REPLY_LEV;
+	private int NOTI_REPLY_NUM;
 	
 	
-	
-	
-	public String getNOTI_MEMBER_NAME() {
-		return NOTI_MEMBER_NAME;
-	}
-	public void setNOTI_MEMBER_NAME(String nOTI_MEMBER_NAME) {
-		NOTI_MEMBER_NAME = nOTI_MEMBER_NAME;
-	}
 	public String getNOTI_MEMBER_ID() {
 		return NOTI_MEMBER_ID;
 	}
 	public void setNOTI_MEMBER_ID(String nOTI_MEMBER_ID) {
 		NOTI_MEMBER_ID = nOTI_MEMBER_ID;
+	}
+	public String getNOTI_MEMBER_NAME() {
+		return NOTI_MEMBER_NAME;
+	}
+	public void setNOTI_MEMBER_NAME(String nOTI_MEMBER_NAME) {
+		NOTI_MEMBER_NAME = nOTI_MEMBER_NAME;
 	}
 	public String getNOTI_SUBJECT() {
 		return NOTI_SUBJECT;
@@ -57,4 +63,54 @@ public class NotiBean {
 	public void setNOTI_NUM(int nOTI_NUM) {
 		NOTI_NUM = nOTI_NUM;
 	}
+	public String getNOTI_REPLY_MEMBER_NAME() {
+		return NOTI_REPLY_MEMBER_NAME;
+	}
+	public void setNOTI_REPLY_MEMBER_NAME(String nOTI_REPLY_MEMBER_NAME) {
+		NOTI_REPLY_MEMBER_NAME = nOTI_REPLY_MEMBER_NAME;
+	}
+	public String getNOTI_REPLY_MEMBER_ID() {
+		return NOTI_REPLY_MEMBER_ID;
+	}
+	public void setNOTI_REPLY_MEMBER_ID(String nOTI_REPLY_MEMBER_ID) {
+		NOTI_REPLY_MEMBER_ID = nOTI_REPLY_MEMBER_ID;
+	}
+	public String getNOTI_REPLY_CONTENT() {
+		return NOTI_REPLY_CONTENT;
+	}
+	public void setNOTI_REPLY_CONTENT(String nOTI_REPLY_CONTENT) {
+		NOTI_REPLY_CONTENT = nOTI_REPLY_CONTENT;
+	}
+	public Date getNOTI_REPLY_DATE() {
+		return NOTI_REPLY_DATE;
+	}
+	public void setNOTI_REPLY_DATE(Date nOTI_REPLY_DATE) {
+		NOTI_REPLY_DATE = nOTI_REPLY_DATE;
+	}
+	public int getNOTI_REPLY_SEQ() {
+		return NOTI_REPLY_SEQ;
+	}
+	public void setNOTI_REPLY_SEQ(int nOTI_REPLY_SEQ) {
+		NOTI_REPLY_SEQ = nOTI_REPLY_SEQ;
+	}
+	public int getNOTI_REPLY_REF() {
+		return NOTI_REPLY_REF;
+	}
+	public void setNOTI_REPLY_REF(int nOTI_REPLY_REF) {
+		NOTI_REPLY_REF = nOTI_REPLY_REF;
+	}
+	public int getNOTI_REPLY_LEV() {
+		return NOTI_REPLY_LEV;
+	}
+	public void setNOTI_REPLY_LEV(int nOTI_REPLY_LEV) {
+		NOTI_REPLY_LEV = nOTI_REPLY_LEV;
+	}
+	public int getNOTI_REPLY_NUM() {
+		return NOTI_REPLY_NUM;
+	}
+	public void setNOTI_REPLY_NUM(int nOTI_REPLY_NUM) {
+		NOTI_REPLY_NUM = nOTI_REPLY_NUM;
+	}
+	
+	
 }
--- web_project/src/com/interior/noti/NotiDAO.java
+++ web_project/src/com/interior/noti/NotiDAO.java
@@ -12,6 +12,7 @@ import javax.naming.InitialContext;
 import javax.servlet.http.HttpSession;
 import javax.sql.DataSource;
 
+
 public class NotiDAO {
 	
 	Connection con;
@@ -269,4 +270,53 @@ public class NotiDAO {
 		}
 		return false;
 	}
+	
+	
+	// 공지사항 글 댓글 쓰기
+			public int NotiReplyWrite(NotiBean notireplywrite){
+				String noti_reply_sql = "select max(noti_reply_num) from noti";
+				String sql="";
+				
+				int num=0;
+				int result=0;
+				
+				int reply_ref = notireplywrite.getNOTI_REPLY_REF();
+				int reply_seq = notireplywrite.getNOTI_REPLY_SEQ();
+				int reply_lev = notireplywrite.getNOTI_REPLY_LEV();
+				
+				try{
+					con=ds.getConnection();
+					pstmt=con.prepareStatement(noti_reply_sql);
+					rs=pstmt.executeQuery();
+					
+					if(rs.next()){
+						num=rs.getInt(1)+1;
+					}else{
+						num=1;
+					}
+					sql = "update NOTI set noti_reply_seq=noti_reply_seq+1 where noti_reply_ref=? and noti_reply_seq>?";
+					
+					pstmt = con.prepareStatement(sql);
+					pstmt.setInt(1, reply_ref);
+					pstmt.setInt(2, reply_seq);
+					
+					sql = "insert into noti (noti_reply_num, noti_reply_member_NAME, noti_reply_content, noti_reply_ref, noti_reply_seq, noti_reply_lev, noti_reply_date) values (noti_reply_seq.nextval,?,?,?,?,?sysdate)";
+					
+					pstmt=con.prepareStatement(sql);
+					pstmt.setString(1, notireplywrite.getNOTI_REPLY_MEMBER_NAME());
+					pstmt.setString(2, notireplywrite.getNOTI_REPLY_CONTENT());
+					pstmt.setInt(3, reply_ref);
+					pstmt.setInt(4, reply_seq);
+					pstmt.setInt(5, reply_lev);
+					pstmt.executeUpdate();
+					return num;
+				}catch(Exception e){
+					System.out.println("NotiReplyWrite error : "+e);
+				}finally{
+					if(rs!=null)try{rs.close();}catch(SQLException e){}
+					if(pstmt!=null)try{pstmt.close();}catch(SQLException e){}
+					if(con!=null)try{con.close();}catch(SQLException e){}
+				}
+				return 0;
+			}
 }
